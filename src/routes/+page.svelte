bd8d82c80a13b7cda2b8a5905117fd5c05df5073
Leon Borowiec <borox@boroxs-MacBook-Air.local>
2024-06-30 23:23:12 +0200

    until i fix everything

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<script>
+<div class="flex items-center justify-center min-h-screen">
+  <h1 class="font-lexend text-7xl font-bold">03.07.24 👨🏻‍💻</h1>
+</div>
 
+<!-- <script>
+  import { scrollTo, scrollRef, scrollTop } from 'svelte-scrolling'
 </script>
 
 <body class="bg-[#101317] font-lexend">
@@ -16,13 +20,13 @@
           When it comes to web development, I find immense joy in working with Svelte and React, my favorite frameworks. They enable me to create dynamic and interactive web experiences with efficiency and elegance. I have strong sense of style when creating UI and UX. Outside the world of coding, I am an avid soccer fan.
         </p>
         <div class="space-x-10">
-          <a href="/contact">
+          <a use:scrollTo={'contact'} href="/">
             <button class="w-[200px] h-[80px] bg-[#14222D] rounded-[10px] font-bold text-white text-xl mt-5">
               Contact me
             </button>
           </a>
           
-          <a href="/contact">
+          <a use:scrollTo={'projects'} href="/">
             <button class="w-[200px] h-[80px] border-[#14222D] border-4 rounded-[10px] font-bold text-white text-xl mt-5">
               My projects
             </button>
@@ -32,21 +36,56 @@
     </div>
   </div>
 
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="mt-[300px]">
-      <h1 class="text-white text-5xl font-extrabold text-center">My projects</h1>
-      <div class="grid grid-cols-1 gap-y-8 mt-8">
-        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#548EBA] bg-size-200 bg-pos-0 hover:bg-pos-100 w-[900px] h-[600px] rounded-[20px] border-2 border-[#19212B] py-28 px-20">
-          <h1 class="font-bold text-white text-5xl pl-6">JustCafe</h1>
-          <p class="text-[#C2BBBB] font-normal pl-6">Website for Cafe.</p>
-          <img src="/thumbnails/project-cafe-thumbnail.png" alt="cafe-thumbnail" />
+  <div class="flex items-center justify-center min-h-screen" >
+    <div class="mt-[300px]" use:scrollRef={'projects'}>
+      <h1 class="text-white text-5xl font-extrabold text-center" >My work</h1>
+      <div class="grid grid-cols-1 gap-y-8 mt-8" >
+        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#548EBA] bg-size-200 bg-pos-0 hover:bg-pos-100 w-[900px] h-[600px] rounded-[20px] border-2 border-[#19212B] py-20 px-20">
+          <h1 class="font-bold text-white text-5xl pl-6">Websites</h1>
+          <p class="text-[#C2BBBB] font-normal pl-6">The "Website for Coffeehouse" project is a dynamic, user-friendly web application designed to enhance a local coffee shop's online presence. Built with Svelte and TailwindCSS, designed in figma it features an interactive menu, intuitive navigation, and visually appealing design, showcasing my ability to create compelling digital experiences. </p>
+          <img src="/thumbnails/websites.png" alt="websites-thumbnail" class="mt-5" />
+        </div>
+
+        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#6d4887] bg-size-200 bg-pos-0 hover:bg-pos-100 w-[900px] h-[600px] rounded-[20px] border-2 border-[#19212B] py-20 px-20">
+          <h1 class="font-bold text-white text-5xl pl-6">Streaming Supplies</h1>
+          <p class="text-[#C2BBBB] font-normal pl-6">The "SomBUD Website" project is a professional site for a construction company, built with Svelte and TailwindCSS, and designed in Figma. It features clean design, intuitive navigation, and detailed project showcases, highlighting my ability to create impactful business websites.</p>
+          <img src="/thumbnails/streaming-supplies.png" alt="cafe-thumbnail" class="mt-5" />
         </div>
       </div>
       <div class="flex items-center justify-center">
-        <button class="w-[200px] h-[80px] border-[#14222D] border-4 rounded-[10px] font-extrabold text-white text-xl mt-5">
-          See more!
-        </button>
+        <a href="https://github.com/borox345">
+          <button class="w-[200px] h-[80px] border-[#14222D] border-4 rounded-[10px] font-extrabold text-white text-xl mt-5">
+            See more!
+          </button>
+        </a>
       </div>
     </div>
   </div>
-</body>
+
+  <div class="flex items-center justify-center min-h-screen" use:scrollRef={'contact'}>
+    <div class="mt-[300px]">
+      <div class="bg-[#15191E] w-[600px] h-[400px] rounded-xl px-8 py-6 gap-x-5 items-center justify-center mt-8 flex">
+        <div>
+          <h1 class="text-white font-extrabold text-4xl">Want to work with me?</h1>
+          <p class="text-[#C2BBBB] font-normal">Feel free to reach out! Whether you have questions, opportunities, or just want to connect, I'd love to hear from you.</p>
+          <div class="flex mt-2">
+						<img src="/icons/discord.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://discordapp.com/users/314424536256872449">borox1337</a>
+					</div>
+
+          <div class="flex">
+						<img src="/icons/mail.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://discordapp.com/users/314424536256872449">borox.biznes@gmail.com</a>
+					</div>
+
+          <div class="flex">
+						<img src="/icons/pay.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://paypal.me/8LB3">paypal.me/8LB3</a>
+					</div>
+          <a href="mailto:borox.tv@gmail.com">
+            <button class="w-[200px] h-[80px] bg-[#14222D] rounded-[10px] font-bold text-white text-xl mt-5">
+              Email
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body> -->

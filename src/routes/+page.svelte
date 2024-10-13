4b8faadba8317004d58a80a561d1450826a16bb6
Leon Borowiec <leon.borowiec@pattlabs.com>
2024-10-13 20:44:22 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,62 @@
-<div class="flex items-center justify-center min-h-screen">
-  <h1 class="font-lexend text-7xl font-bold">03.07.24 👨🏻‍💻</h1>
-</div>
-
-<!-- <script>
+<script>
   import { scrollTo, scrollRef, scrollTop } from 'svelte-scrolling'
 </script>
 
 <body class="bg-[#101317] font-lexend">
   <div class="flex items-center justify-center min-h-screen">
-    <div class="flex gap-x-10">
-      <img src="/images/av-rec.png" alt="avatar" class="w-[450px]"/>
+    <div class="lg:flex gap-x-10 px-10 items-center justify-center">
+      <img src="/images/av-rec.png" alt="avatar" class="lg:w-[450px] lg:h-[450px] w-[100px] h-[100px]"/>
       <div>
-        <h1 class="text-white text-7xl font-extrabold">Hello!</h1>
-        <span class="font-extrabold text-white">17 // py & web fullstack</span>
+        <h1 class="text-white text-7xl font-extrabold mt-2">Hello!</h1>
+        <span class="font-extrabold text-white">17 // Software Engineer @ Patterson Labs, LLC</span>
         <p class="text-[#C2BBBB] text-md max-w-xl">
-          I'm <span class="font-bold">Leon</span>, a student from Poland with a passion for software development! My journey began five years ago, and since then, I have delved into numerous programming languages, mastering each one to expand my skills and knowledge. My expertise shines in developing applications using Python, where I have accumulated extensive experience and honed my abilities.
-          <br/>
-          <br/>
-          When it comes to web development, I find immense joy in working with Svelte and React, my favorite frameworks. They enable me to create dynamic and interactive web experiences with efficiency and elegance. I have strong sense of style when creating UI and UX. Outside the world of coding, I am an avid soccer fan.
+          I'm <span class="font-bold">Leon</span>, a student from Poland with a passion for software development!
+          <span class="font-bold">I'm currently working on:</span> <a class="text-blue-400 hover:underline" href="https://firepanel.r3dlabs.com">FirePanel</a> 🔥 for <a class="text-blue-400 hover:underline" href="https://r3dlabs.com">R3dLabs</a>
         </p>
-        <div class="space-x-10">
-          <a use:scrollTo={'contact'} href="/">
+        <div>
+          <div class="flex mt-2">
+						<img src="/icons/discord.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://discordapp.com/users/314424536256872449">borox1337</a>
+					</div>
+
+          <div class="flex">
+						<img src="/icons/mail.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="mailto:borox.biznes@gmail.com">leon.borowiec@gmail.com</a>
+					</div>
+
+          <div class="flex">
+						<img src="/icons/pay.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://paypal.me/8LB3">paypal.me/8LB3</a>
+					</div>
+        </div>
+        <div class="lg:space-x-10">
+          <!-- <a use:scrollTo={'contact'} href="/">
             <button class="w-[200px] h-[80px] bg-[#14222D] rounded-[10px] font-bold text-white text-xl mt-5">
               Contact me
             </button>
-          </a>
+          </a> -->
           
-          <a use:scrollTo={'projects'} href="/">
+          <!-- <a use:scrollTo={'projects'} href="/">
             <button class="w-[200px] h-[80px] border-[#14222D] border-4 rounded-[10px] font-bold text-white text-xl mt-5">
               My projects
             </button>
-          </a>
+          </a> -->
         </div>
       </div>
     </div>
   </div>
 
-  <div class="flex items-center justify-center min-h-screen" >
+  <!-- <div class="flex items-center justify-center min-h-screen" >
     <div class="mt-[300px]" use:scrollRef={'projects'}>
       <h1 class="text-white text-5xl font-extrabold text-center" >My work</h1>
-      <div class="grid grid-cols-1 gap-y-8 mt-8" >
-        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#548EBA] bg-size-200 bg-pos-0 hover:bg-pos-100 w-[900px] h-[600px] rounded-[20px] border-2 border-[#19212B] py-20 px-20">
+      <div class="grid grid-cols-1 gap-y-8 mt-8 px-20">
+        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#3E70DD] bg-size-200 bg-pos-0 hover:bg-pos-100 lg:w-[900px] w-full lg:h-[600px] h-[400px] rounded-[20px] border-2 border-[#19212B] lg:py-20 py-10 px-20">
           <h1 class="font-bold text-white text-5xl pl-6">Websites</h1>
-          <p class="text-[#C2BBBB] font-normal pl-6">The "Website for Coffeehouse" project is a dynamic, user-friendly web application designed to enhance a local coffee shop's online presence. Built with Svelte and TailwindCSS, designed in figma it features an interactive menu, intuitive navigation, and visually appealing design, showcasing my ability to create compelling digital experiences. </p>
-          <img src="/thumbnails/websites.png" alt="websites-thumbnail" class="mt-5" />
+          <p class="text-[#C2BBBB] font-normal pl-6">Here you can find some of my projects related to web development. Each website/design with description. </p>
+          <img src="/thumbnails/web2.png" alt="websites-thumbnail" class="mt-5" />
         </div>
 
-        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#6d4887] bg-size-200 bg-pos-0 hover:bg-pos-100 w-[900px] h-[600px] rounded-[20px] border-2 border-[#19212B] py-20 px-20">
+        <div class="bg-[#15191E] transition-all duration-500 bg-gradient-to-t to-[#15191E] via-[#15191E] from-[#263654] bg-size-200 bg-pos-0 hover:bg-pos-100 lg:w-[900px] w-full lg:h-[600px] h-[400px] rounded-[20px] border-2 border-[#19212B] lg:py-20 py-10 px-20">
           <h1 class="font-bold text-white text-5xl pl-6">Streaming Supplies</h1>
-          <p class="text-[#C2BBBB] font-normal pl-6">The "SomBUD Website" project is a professional site for a construction company, built with Svelte and TailwindCSS, and designed in Figma. It features clean design, intuitive navigation, and detailed project showcases, highlighting my ability to create impactful business websites.</p>
-          <img src="/thumbnails/streaming-supplies.png" alt="cafe-thumbnail" class="mt-5" />
+          <p class="text-[#C2BBBB] font-normal pl-6">Technical services for streamers. Twitch extensions, chatbots, interactive widgets, and anything that can improve their stream. </p>
+          <img src="/thumbnails/streaming-supplies.png" alt="websites-thumbnail" class="mt-5" />
         </div>
       </div>
       <div class="flex items-center justify-center">
@@ -60,11 +67,11 @@
         </a>
       </div>
     </div>
-  </div>
+  </div> -->
 
-  <div class="flex items-center justify-center min-h-screen" use:scrollRef={'contact'}>
-    <div class="mt-[300px]">
-      <div class="bg-[#15191E] w-[600px] h-[400px] rounded-xl px-8 py-6 gap-x-5 items-center justify-center mt-8 flex">
+  <!-- <div class="flex items-center justify-center min-h-screen" use:scrollRef={'contact'}>
+    <div class="">
+      <div class="bg-[#15191E] w-[600px] h-[250px] rounded-xl px-8 py-6 gap-x-5 items-center justify-center mt-8 flex">
         <div>
           <h1 class="text-white font-extrabold text-4xl">Want to work with me?</h1>
           <p class="text-[#C2BBBB] font-normal">Feel free to reach out! Whether you have questions, opportunities, or just want to connect, I'd love to hear from you.</p>
@@ -73,19 +80,14 @@
 					</div>
 
           <div class="flex">
-						<img src="/icons/mail.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://discordapp.com/users/314424536256872449">borox.biznes@gmail.com</a>
+						<img src="/icons/mail.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="mailto:borox.biznes@gmail.com">leon.borowiec@gmail.com</a>
 					</div>
 
           <div class="flex">
 						<img src="/icons/pay.svg" alt="Localization icon" class="mr-2 w-6 h-6"><a class="text-graynew font-bold hover:underline text-[#C2BBBB]" href="https://paypal.me/8LB3">paypal.me/8LB3</a>
 					</div>
-          <a href="mailto:borox.tv@gmail.com">
-            <button class="w-[200px] h-[80px] bg-[#14222D] rounded-[10px] font-bold text-white text-xl mt-5">
-              Email
-            </button>
-          </a>
         </div>
       </div>
     </div>
-  </div>
-</body> -->
+  </div> -->
+</body>
---
+++ src/routes/projects/[project]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+  // import { getChantById } from "$lib/firebaseService.js";
+
+  export let data;
+  let id = data.work;
+  let content = {};
+  /**
+   * @type {{ title: any; content: any; } | null}
+   */
+  let chant;
+
+  onMount(async () => {
+    // const chantPromise = getChantById("chants", id); // Example usage
+    // chant = await getChantById("chants", id);
+    // console.log(chant);
+  });
+</script>
+<body class="bg-[#101317] font-lexend">
+  <div class="relative px-36">
+    <h1 class="text-white text-7xl font-extrabold mt-2">Hello {id}!</h1>
+  </div>
+</body>
